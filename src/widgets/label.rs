111e13217f6f6e53736609b6883870335371ea4d
Alexander Khudekov <pancir3d@steptosky.com>
2023-02-24 21:16:20 +0300

    wip

--- src/widgets/label.rs
+++ src/widgets/label.rs
@@ -1,14 +1,39 @@
-use sim_draw::m::Rect;
-use sim_draw::{TextAlign, TextPaint};
+use sim_draw::m::{Box2, Point2};
+use sim_draw::{Canvas, TextAlign, TextPaint};
 use std::borrow::Cow;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub struct Label {
-   rect: Rect<f32>,
-   text: Cow<'static, str>,
-   paint: TextPaint,
-   align: TextAlign,
+   pub pos: Point2<f32>,
+   pub text: Cow<'static, str>,
+   pub paint: TextPaint,
+   pub align: TextAlign,
+}
+
+impl Default for Label {
+   fn default() -> Self {
+      Self {
+         pos: Point2::new(0.0, 0.0),
+         text: Cow::Borrowed(""),
+         paint: Default::default(),
+         align: Default::default(),
+      }
+   }
+}
+
+impl Label {
+   pub fn new<TXT>(text: TXT, pos: Point2<f32>, paint: TextPaint, align: TextAlign) -> Self
+   where
+      TXT: Into<Cow<'static, str>>,
+   {
+      Self { pos, text: text.into(), paint, align }
+   }
+
+   pub fn draw(&self, canvas: &mut Canvas) -> Box2<f32> {
+      canvas.set_text_paint(self.paint.clone());
+      canvas.text_line(self.text.as_ref(), self.pos, self.align)
+   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
