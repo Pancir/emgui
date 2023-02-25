7ab42caccd5b1ed2668164ece09d3981addd05b2
Alexander Khudekov <pancir3d@steptosky.com>
2023-02-25 16:48:11 +0300

    wip

--- src/experiments/push_button.rs
+++ src/experiments/push_button.rs
@@ -6,19 +6,12 @@ use std::rc::{Rc, Weak};
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub struct PushButton {
-   self_ref: Option<Weak<RefCell<Widget<Self>>>>,
+   self_ref: Weak<RefCell<Widget<Self>>>,
 }
 
 impl PushButton {
    pub fn new() -> Rc<RefCell<Widget<Self>>> {
-      let s = Self { self_ref: None };
-
-      let mut out = Widget::new(s, |vt| {});
-
-      let self_ref = Rc::downgrade(&out);
-      out.borrow_mut().derive.self_ref = Some(self_ref);
-
-      out
+      Widget::new(|_vt, self_ref| Self { self_ref })
    }
 }
 
@@ -27,8 +20,8 @@ impl Derive for PushButton {
       self
    }
 
-   fn set_ref(&mut self, self_ref: Weak<RefCell<Widget<Self>>>) {
-      self.self_ref = Some(self_ref);
+   fn as_any_mut(&mut self) -> &mut dyn Any {
+      self
    }
 }
 
--- src/experiments/test_3.rs
+++ src/experiments/test_3.rs
@@ -1,6 +1,8 @@
+use crate::widgets::WidgetId;
 use sim_draw::Canvas;
 use std::any::Any;
 use std::cell::RefCell;
+use std::mem::MaybeUninit;
 use std::ops::DerefMut;
 use std::rc::{Rc, Weak};
 
@@ -8,6 +10,8 @@ use std::rc::{Rc, Weak};
 
 pub trait Derive: Any + 'static {
    fn as_any(&self) -> &dyn Any;
+   fn as_any_mut(&mut self) -> &mut dyn Any;
+
    fn derive_void(&self) -> Option<&dyn Derive> {
       None
    }
@@ -48,7 +52,17 @@ impl Children {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/// Cast dyn [IWidget] to specified type.
+pub fn cast<T: Derive>(_input: Weak<RefCell<&dyn IWidget>>) -> Weak<RefCell<Widget<T>>> {
+   unimplemented!()
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 pub trait IWidget: Any + 'static {
+   fn as_any(&self) -> &dyn Any;
+   fn as_any_mut(&mut self) -> &mut dyn Any;
+
    /// Get the widget type name for debugging purposes.
    /// Developers should not override this method.
    fn type_name(&self) -> &'static str {
@@ -63,9 +77,19 @@ pub trait IWidget: Any + 'static {
       name.split('<').next().unwrap_or(name).split("::").last().unwrap_or(name)
    }
 
+   fn id(&self) -> WidgetId;
+
    //---------------------------------------
 
+   fn derive(&self) -> &dyn Derive;
+
+   fn derive_mut(&mut self) -> &mut dyn Derive;
+
    fn children(&self) -> &Children;
+
+   fn request_draw(&mut self);
+
+   fn request_delete(&mut self);
 
    fn is_visible(&self) -> bool;
 
@@ -87,29 +111,56 @@ pub struct Widget<D>
 where
    D: Derive,
 {
-   derive: D,
+   id: WidgetId,
+   derive: MaybeUninit<D>,
    vtable: WidgetVt<Self>,
    children: Children,
+
+   parent: Option<Weak<RefCell<dyn IWidget>>>,
+   needs_draw: bool,
+   needs_del: bool,
 }
 
 impl<D: 'static> Widget<D>
 where
    D: Derive,
 {
-   pub fn new(derive: D, cb: impl Fn(&mut WidgetVt<Self>)) -> Rc<RefCell<Self>> {
-      let mut out = Self {
-         derive,
+   pub fn new<CB>(cb: CB) -> Rc<RefCell<Self>>
+   where
+      CB: Fn(&mut WidgetVt<Self>, Weak<RefCell<Widget<D>>>) -> D,
+   {
+      let out = Self {
+         id: WidgetId::new(),
+         derive: unsafe { std::mem::zeroed() },
          vtable: WidgetVt { on_draw: Self::on_draw },
          children: Children::default(),
+         //-----------
+         parent: None,
+         needs_draw: false,
+         needs_del: false,
       };
 
-      cb(&mut out.vtable);
       let out = Rc::new(RefCell::new(out));
-      let self_ref = Rc::downgrade(&out);
-
-      out.borrow_mut().derive.set_ref(self_ref);
+      {
+         let self_ref = Rc::downgrade(&out);
+         let mut bor = out.borrow_mut();
+         let derive = cb(&mut bor.vtable, self_ref);
+         bor.derive.write(derive);
+      }
 
       out
+   }
+
+   pub fn derive_ref(&self) -> &D {
+      // # Safety
+      // All initialization happen in new function.
+      unsafe { self.derive.assume_init_ref() }
+   }
+
+   pub fn derive_mut(&mut self) -> &mut D {
+      // # Safety
+      // All initialization happen in new function.
+      unsafe { self.derive.assume_init_mut() }
    }
 }
 
@@ -117,17 +168,55 @@ impl<D: 'static> IWidget for Widget<D>
 where
    D: Derive,
 {
+   fn as_any(&self) -> &dyn Any {
+      self
+   }
+
+   fn as_any_mut(&mut self) -> &mut dyn Any {
+      self
+   }
+
+   fn id(&self) -> WidgetId {
+      self.id
+   }
+
+   fn derive(&self) -> &dyn Derive {
+      self.derive_ref()
+   }
+
+   fn derive_mut(&mut self) -> &mut dyn Derive {
+      self.derive_mut()
+   }
+
    fn children(&self) -> &Children {
       &self.children
+   }
+
+   fn request_draw(&mut self) {
+      if !self.needs_draw {
+         self.needs_draw = true;
+         if let Some(p) = &self.parent {
+            if let Some(o) = p.upgrade() {
+               o.borrow_mut().request_draw();
+            }
+         }
+      }
+   }
+
+   fn request_delete(&mut self) {
+      self.needs_del = true;
    }
 
    fn is_visible(&self) -> bool {
       true
    }
 
-   fn on_draw(&mut self, _canvas: &mut Canvas) {}
+   fn on_draw(&mut self, canvas: &mut Canvas) {
+      (self.vtable.on_draw)(self, canvas)
+   }
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn on_draw(child: &Rc<RefCell<dyn IWidget>>, canvas: &mut Canvas, force: bool) {
