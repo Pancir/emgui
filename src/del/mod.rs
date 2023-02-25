8527713744886f7edca626f1117166cca04f4ba2
Alexander Khudekov <pancir3d@steptosky.com>
2023-02-25 13:34:40 +0300

    wip

--- src/del/mod.rs
+++ src/del/mod.rs
@@ -1,8 +1,10 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub trait SetupDerive<REF, VT> {
-   fn setup_ref(&mut self, vt: REF);
-   fn setup_vtable(&mut self, vt: &mut VT);
+pub trait SetupDerive<REF> {
+   fn setup_derive(&mut self, vt: Weak<RefCell<REF>>);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -13,29 +15,70 @@ pub struct BaseVTable<D> {
 
 pub struct Base<D>
 where
-   D: SetupDerive<Self, BaseVTable<Self>>,
+   D: SetupDerive<Self>,
 {
+   pub derive: D,
    vtable: BaseVTable<Self>,
 }
 
 impl<D> Base<D>
 where
-   D: SetupDerive<Self, BaseVTable<Self>>,
+   D: SetupDerive<Self>,
 {
-   pub fn new() -> Self {
-      Self { vtable: BaseVTable { on_test: Self::on_test } }
+   pub fn new(derive: D) -> Rc<RefCell<Self>> {
+      let out =
+         Rc::new(RefCell::new(Self { derive, vtable: BaseVTable { on_test: Self::on_test } }));
+      let w = Rc::downgrade(&out);
+      match out.try_borrow_mut() {
+         Ok(mut r) => r.derive.setup_derive(w),
+         Err(_) => {
+            unreachable!()
+         }
+      }
+      out
    }
 
-   fn on_test(b: &mut Self) {}
+   fn on_test(_b: &mut Self) {}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-pub struct Derive {}
+pub struct DeriveGav {
+   self_ref: Weak<RefCell<Base<Self>>>,
+}
 
-impl SetupDerive<Base<Self>, BaseVTable<Base<Self>>> for Derive {
-   fn setup_ref(&mut self, vt: Base<Self>) {}
-   fn setup_vtable(&mut self, vt: &mut BaseVTable<Base<Self>>) {}
+impl DeriveGav {
+   fn on_test(_b: &mut Base<Self>) {}
+}
+
+impl SetupDerive<Base<Self>> for DeriveGav {
+   fn setup_derive(&mut self, base: Weak<RefCell<Base<Self>>>) {
+      self.self_ref = base;
+      if let Some(s) = self.self_ref.upgrade() {
+         let mut b = s.borrow_mut();
+         b.vtable.on_test = Self::on_test;
+      }
+   }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub struct DeriveGavGav {
+   self_ref: Weak<RefCell<Base<Self>>>,
+}
+
+impl DeriveGavGav {
+   fn on_test(_b: &mut Base<Self>) {}
+}
+
+impl SetupDerive<Base<Self>> for DeriveGavGav {
+   fn setup_derive(&mut self, base: Weak<RefCell<Base<Self>>>) {
+      self.self_ref = base;
+      if let Some(s) = self.self_ref.upgrade() {
+         let mut b = s.borrow_mut();
+         b.vtable.on_test = Self::on_test;
+      }
+   }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
