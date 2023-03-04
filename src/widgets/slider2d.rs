4b4a55c3b126933300949975fd788f2a2a968583
Alexander Khudekov <pancir3d@steptosky.com>
2023-03-04 11:22:41 +0300

    wip

--- src/widgets/slider2d.rs
+++ src/widgets/slider2d.rs
@@ -7,8 +7,33 @@ use sim_draw::{Canvas, Paint};
 use sim_input::mouse::{MouseButton, MouseState};
 use std::any::Any;
 use std::cell::RefCell;
+use std::ops::Range;
 use std::rc::Rc;
-use std::time::{Duration, Instant};
+use std::time::Instant;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Slider value with range.
+#[derive(Clone)]
+pub struct SliderValue {
+   pub range: Range<i32>,
+   pub value: i32,
+}
+
+impl Default for SliderValue {
+   fn default() -> Self {
+      Self { range: Range { start: 0, end: 100 }, value: 0 }
+   }
+}
+
+impl SliderValue {
+   /// Convert value to range of `0.0..=1.0`.
+   #[inline]
+   pub fn unit_interval(&self) -> f32 {
+      debug_assert!(self.range.start != self.range.end, "Range can't be 0");
+      (self.value - self.range.start) as f32 / (self.range.end - self.range.start) as f32
+   }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -16,17 +41,17 @@ pub trait ISlider2dHandler {
    /// This is called  when [Slider2dState::is_down] is `true` and the slider moves.
    ///
    /// This usually happens when the user is dragging the slider.
-   fn slider_moved(&mut self, _x: f32, _y: f32) {}
+   fn slider_moved(&mut self, _x: SliderValue, _y: SliderValue) {}
 
    /// This is called when the user presses the slider with the mouse.
    ///
    /// The [Slider2dState::is_down] is `true`.
-   fn slider_pressed(&mut self, _x: f32, _y: f32) {}
+   fn slider_pressed(&mut self, _x: SliderValue, _y: SliderValue) {}
 
    /// This is called when the user releases the slider with the mouse.
    ///
    /// The [Slider2dState::is_down] is `false`.
-   fn slider_released(&mut self, _x: f32, _y: f32) {}
+   fn slider_released(&mut self, _x: SliderValue, _y: SliderValue) {}
 }
 
 /// Default Slider handler.
@@ -38,9 +63,9 @@ pub trait ISlider2dHandler {
 /// Heap allocation happens only when you add a closure.
 #[derive(Default)]
 pub struct Slider2dHandler {
-   on_slider_moved: Option<Box<dyn FnMut(f32, f32)>>,
-   on_slider_pressed: Option<Box<dyn FnMut(f32, f32)>>,
-   on_slider_released: Option<Box<dyn FnMut(f32, f32)>>,
+   on_slider_moved: Option<Box<dyn FnMut(SliderValue, SliderValue)>>,
+   on_slider_pressed: Option<Box<dyn FnMut(SliderValue, SliderValue)>>,
+   on_slider_released: Option<Box<dyn FnMut(SliderValue, SliderValue)>>,
 }
 
 impl Slider2dHandler {
@@ -52,14 +77,14 @@ impl Slider2dHandler {
    /// Set callback.
    ///
    /// It allocates memory in heap for the closure.
-   pub fn on_slider_moved(mut self, cb: impl FnMut(f32, f32) + 'static) -> Self {
+   pub fn on_slider_moved(mut self, cb: impl FnMut(SliderValue, SliderValue) + 'static) -> Self {
       self.on_slider_moved = Some(Box::new(cb));
       self
    }
    /// Set callback.
    ///
    /// It allocates memory in heap for the closure.
-   pub fn on_slider_pressed(mut self, cb: impl FnMut(f32, f32) + 'static) -> Self {
+   pub fn on_slider_pressed(mut self, cb: impl FnMut(SliderValue, SliderValue) + 'static) -> Self {
       self.on_slider_pressed = Some(Box::new(cb));
       self
    }
@@ -67,26 +92,26 @@ impl Slider2dHandler {
    /// Set callback.
    ///
    /// It allocates memory in heap for the closure.
-   pub fn on_slider_released(mut self, cb: impl FnMut(f32, f32) + 'static) -> Self {
+   pub fn on_slider_released(mut self, cb: impl FnMut(SliderValue, SliderValue) + 'static) -> Self {
       self.on_slider_released = Some(Box::new(cb));
       self
    }
 }
 
 impl ISlider2dHandler for Slider2dHandler {
-   fn slider_moved(&mut self, x: f32, y: f32) {
+   fn slider_moved(&mut self, x: SliderValue, y: SliderValue) {
       if let Some(h) = &mut self.on_slider_moved {
          (h)(x, y)
       }
    }
 
-   fn slider_pressed(&mut self, x: f32, y: f32) {
+   fn slider_pressed(&mut self, x: SliderValue, y: SliderValue) {
       if let Some(h) = &mut self.on_slider_pressed {
          (h)(x, y)
       }
    }
 
-   fn slider_released(&mut self, x: f32, y: f32) {
+   fn slider_released(&mut self, x: SliderValue, y: SliderValue) {
       if let Some(h) = &mut self.on_slider_released {
          (h)(x, y)
       }
@@ -97,17 +122,17 @@ impl ISlider2dHandler for Slider2dHandler {
 
 #[derive(Default)]
 pub struct Slider2dState {
-   /// Current X value from `0.0..=1.0`
-   pub value_x: f32,
+   /// X value.
+   pub x: SliderValue,
 
-   /// Current Y value from `0.0..=1.0`
-   pub value_y: f32,
+   /// Y value.
+   pub y: SliderValue,
 
-   /// Default [Self::value_x] that is used when the slider is reset.
-   pub default_value_x: f32,
+   /// Default [Self::x] that is used when the slider is reset.
+   pub default_x: i32,
 
-   /// Default [Self::value_y] that is used when the slider is reset.
-   pub default_value_y: f32,
+   /// Default [Self::y] that is used when the slider is reset.
+   pub default_y: i32,
 
    //---------------------------
    /// Mouse pressed on handle.
@@ -158,7 +183,11 @@ where
    H: ISlider2dHandler + 'static,
 {
    pub fn new(handler: H, rect: Rect<f32>) -> Rc<RefCell<Widget<Self>>> {
-      Widget::new(
+      Self::new_flat(handler, rect).to_rc()
+   }
+
+   pub fn new_flat(handler: H, rect: Rect<f32>) -> Widget<Self> {
+      Widget::new_flat(
          |vt| {
             vt.on_draw = Self::on_draw;
             vt.on_set_rect = Self::on_set_rect;
@@ -172,10 +201,10 @@ where
                click_pos: Point2::ZERO,
                released_at: Instant::now(),
                state: Slider2dState {
-                  value_x: 0.5,
-                  value_y: 0.5,
-                  default_value_x: 0.5,
-                  default_value_y: 0.5,
+                  x: SliderValue::default(),
+                  y: SliderValue::default(),
+                  default_x: 50,
+                  default_y: 50,
                   is_handle_down: false,
                   is_over_handle: false,
                   grab_area: Box2::ZERO,
@@ -191,14 +220,92 @@ where
       )
    }
 
-   pub fn set_handle_current_pos(&mut self, x: f32, y: f32) {
-      self.state.value_x = x.clamp(0.0, 1.0);
-      self.state.value_y = y.clamp(0.0, 1.0);
+   pub fn to_rc(self) -> Rc<RefCell<Self>> {
+      Rc::new(RefCell::new(self))
    }
 
-   pub fn set_handle_default_pos(&mut self, x: f32, y: f32) {
-      self.state.default_value_x = x.clamp(0.0, 1.0);
-      self.state.default_value_y = y.clamp(0.0, 1.0);
+   //----------------------------------------------------------
+
+   #[inline]
+   pub fn set(&mut self, r_x: Range<i32>, r_y: Range<i32>, v_x: i32, v_y: i32, d_x: i32, d_y: i32) {
+      self.set_ranges(r_x, r_y);
+      self.set_values(v_x, v_y);
+      self.set_default_values(d_x, d_y);
+   }
+
+   //----------------------------------------------------------
+
+   #[inline]
+   pub fn set_ranges(&mut self, x: Range<i32>, y: Range<i32>) {
+      self.set_x_range(x);
+      self.set_y_range(y);
+   }
+
+   #[inline]
+   pub fn set_x_range(&mut self, range: Range<i32>) {
+      self.state.x.range = range;
+      self.state.x.value =
+         self.state.x.value.clamp(self.state.x.range.start, self.state.x.range.end);
+   }
+
+   #[inline]
+   pub fn set_y_range(&mut self, range: Range<i32>) {
+      self.state.y.range = range;
+      self.state.y.value =
+         self.state.y.value.clamp(self.state.y.range.start, self.state.y.range.end);
+   }
+
+   #[inline]
+   pub fn ranges(&self) -> (Range<i32>, Range<i32>) {
+      (self.state.x.range.clone(), self.state.y.range.clone())
+   }
+
+   //----------------------------------------------------------
+
+   #[inline]
+   pub fn set_values(&mut self, x: i32, y: i32) {
+      self.set_x_value(x);
+      self.set_y_value(y);
+   }
+
+   #[inline]
+   pub fn set_x_value(&mut self, value: i32) {
+      self.state.x.value = value.clamp(self.state.x.range.start, self.state.x.range.end);
+      self.set_handle_position_x(self.state.x.unit_interval());
+   }
+
+   #[inline]
+   pub fn set_y_value(&mut self, value: i32) {
+      self.state.y.value = value.clamp(self.state.y.range.start, self.state.y.range.end);
+      self.set_handle_position_y(self.state.y.unit_interval());
+   }
+
+   #[inline]
+   pub fn values(&self) -> (i32, i32) {
+      (self.state.x.value, self.state.y.value)
+   }
+
+   //----------------------------------------------------------
+
+   #[inline]
+   pub fn set_default_values(&mut self, x: i32, y: i32) {
+      self.set_default_x_value(x);
+      self.set_default_y_value(y);
+   }
+
+   #[inline]
+   pub fn set_default_x_value(&mut self, value: i32) {
+      self.state.default_x = value;
+   }
+
+   #[inline]
+   pub fn set_default_y_value(&mut self, value: i32) {
+      self.state.default_y = value;
+   }
+
+   #[inline]
+   pub fn default_values(&self) -> (i32, i32) {
+      (self.state.default_x, self.state.default_y)
    }
 }
 
@@ -206,11 +313,11 @@ impl<H> Slider2d<H>
 where
    H: ISlider2dHandler + 'static,
 {
-   pub fn map_to_unit(start: f32, end: f32, v: f32) -> f32 {
+   fn map_to_unit(start: f32, end: f32, v: f32) -> f32 {
       (v - start) / (end - start)
    }
 
-   fn read_handle_position(&self) -> (f32, f32) {
+   fn get_handle_position(&self) -> (f32, f32) {
       let width = self.state.grab_area.width();
       let height = self.state.grab_area.height();
 
@@ -237,18 +344,44 @@ where
       (x, y)
    }
 
-   fn write_handle_position(&mut self, x: f32, y: f32) {
+   fn set_handle_position_x(&mut self, x: f32) {
       debug_assert!((0.0..=1.0).contains(&x));
-      debug_assert!((0.0..=1.0).contains(&y));
+      self.state.handle_position.x = self.state.grab_area.min.x + self.state.grab_area.width() * x;
+   }
 
-      let x_offset = self.state.grab_area.width() * x;
-      let y_offset = self.state.grab_area.height() * y;
-      self.state.handle_position =
-         Point2::new(self.state.grab_area.min.x + x_offset, self.state.grab_area.min.y + y_offset)
+   fn set_handle_position_y(&mut self, y: f32) {
+      debug_assert!((0.0..=1.0).contains(&y));
+      self.state.handle_position.y = self.state.grab_area.min.y + self.state.grab_area.height() * y;
+   }
+
+   fn set_handle_position(&mut self, x: f32, y: f32) {
+      self.set_handle_position_x(x);
+      self.set_handle_position_y(y);
+   }
+
+   fn set_handle_value_to_values(&mut self, v: (f32, f32)) {
+      debug_assert!(self.state.x.range.start != self.state.x.range.end, "range can't be 0");
+      debug_assert!(self.state.y.range.start != self.state.y.range.end, "range can't be 0");
+
+      self.state.x.value = ((self.state.x.range.end - self.state.x.range.start) as f32 * v.0)
+         as i32
+         + self.state.x.range.start;
+
+      self.state.y.value = ((self.state.y.range.end - self.state.y.range.start) as f32 * v.1)
+         as i32
+         + self.state.y.range.start;
    }
 
    fn reset_handle_position(&mut self) {
-      self.write_handle_position(self.state.default_value_x, self.state.default_value_y)
+      // Default can't be out of range.
+
+      let default_x = self.state.default_x.clamp(self.state.x.range.start, self.state.x.range.end);
+      let default_y = self.state.default_y.clamp(self.state.y.range.start, self.state.y.range.end);
+
+      let x = SliderValue { range: self.state.x.range.clone(), value: default_x };
+      let y = SliderValue { range: self.state.y.range.clone(), value: default_y };
+
+      self.set_handle_position(x.unit_interval(), y.unit_interval())
    }
 }
 
@@ -263,7 +396,7 @@ where
       let t_off = d.state.handle_rect.y;
       let b_off = d.state.handle_rect.height + d.state.handle_rect.y;
       d.state.grab_area = rect.margin(l_off, -r_off, t_off, -b_off).into();
-      d.write_handle_position(d.state.value_x, d.state.value_y);
+      d.set_handle_position(d.state.x.unit_interval(), d.state.y.unit_interval());
 
       Some(rect)
    }
@@ -296,19 +429,26 @@ where
       let mut d = w.derive_mut();
 
       if d.state.is_handle_down {
+         // TODO Snap vert/hor.
+         // if event.input.modifiers.shift() {
          let mouse_pos = Point2::new(event.input.x, event.input.y);
 
          d.state.handle_position =
             (mouse_pos - d.click_pos).clamp(d.state.grab_area.min, d.state.grab_area.max);
 
-         let (x, y) = d.read_handle_position();
-         d.handler.slider_moved(x, y);
+         d.set_handle_value_to_values(d.get_handle_position());
+         d.handler.slider_moved(d.state.x.clone(), d.state.y.clone());
 
          w.request_draw();
+         // }
       } else {
          let h_rect = d.state.handle_rect.offset(d.state.handle_position);
-         d.state.is_over_handle = h_rect.is_inside(event.input.x, event.input.y);
-         w.request_draw();
+         let is_inside = h_rect.is_inside(event.input.x, event.input.y);
+
+         if is_inside != d.state.is_over_handle {
+            d.state.is_over_handle = is_inside;
+            w.request_draw();
+         }
       }
       true
    }
@@ -331,7 +471,7 @@ where
                let mut d = w.derive_mut();
                d.click_pos = Point2::new(event.input.x, event.input.y) - d.state.handle_position;
                d.state.is_handle_down = true;
-               d.handler.slider_pressed(d.state.value_x, d.state.value_y);
+               d.handler.slider_pressed(d.state.x.clone(), d.state.y.clone());
                w.request_draw();
             }
          }
@@ -339,7 +479,7 @@ where
             let mut d = w.derive_mut();
             if d.state.is_handle_down {
                d.state.is_handle_down = false;
-               d.handler.slider_released(d.state.value_x, d.state.value_y);
+               d.handler.slider_released(d.state.x.clone(), d.state.y.clone());
 
                if is_inside_handle {
                   let db_time = w.double_click_time();
@@ -348,8 +488,8 @@ where
                   if d.released_at.elapsed() < db_time {
                      d.reset_handle_position();
 
-                     let (x, y) = d.read_handle_position();
-                     d.handler.slider_moved(x, y);
+                     d.set_handle_value_to_values(d.get_handle_position());
+                     d.handler.slider_moved(d.state.x.clone(), d.state.y.clone());
                   }
                }
 
